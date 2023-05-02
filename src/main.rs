25f1f3ad2b4ad41aa5fc7b653a83b8991bc48153
Manan Karnik <manankarnik03@gmail.com>
2023-05-02 21:38:51 +0530

    refactor: move configuration to noise map struct

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,23 @@
 use bevy::prelude::*;
-use bevy_generative::noise_map::{Method, NoiseMap, NoiseMapConfig, NoiseMapPlugin};
+use bevy_generative::noise_map::{Method, NoiseMap, NoiseMapPlugin};
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugin(NoiseMapPlugin)
-        .insert_resource(NoiseMapConfig {
-            method: Method::OpenSimplex,
-            scale: 0.04,
-            size: [200; 2],
-            seed: 0,
-        })
         .add_startup_system(setup)
         .run();
 }
 
 fn setup(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
-    commands.spawn((ImageBundle::default(), NoiseMap));
+    commands.spawn((
+        ImageBundle::default(),
+        NoiseMap {
+            method: Method::OpenSimplex,
+            scale: 0.04,
+            size: [400; 2],
+            seed: 0,
+        },
+    ));
 }
--- src/noise_map.rs
+++ src/noise_map.rs
@@ -1,8 +1,9 @@
-//! Generate noise map. For configuration, see [NoiseMapConfig](./struct.NoiseMapConfig.html)
+//! Generate noise map
 //! # Example
+//! For configuration, see [`NoiseMap`](./struct.NoiseMap.html)
 //! ```
 //! use bevy::prelude::*;
-//! use bevy_generative::noise_map::NoiseMapPlugin;
+//! use bevy_generative::noise_map::{NoiseMap, NoiseMapPlugin};
 //!
 //! fn main() {
 //!     App::new()
@@ -14,28 +15,45 @@
 //!
 //! fn setup(mut commands: Commands) {
 //!     commands.spawn(Camera2dBundle::default());
+//!     commands.spawn((ImageBundle::default(), NoiseMap::default()));
 //! }
 //! ```
-use bevy::{
-    prelude::*,
-    render::render_resource::{Extent3d, TextureDimension, TextureFormat},
-};
-use image::DynamicImage;
+use bevy::{prelude::*, render::render_resource::TextureFormat};
 
 use crate::noise::generate_noise_by_method;
 
-/// Plugin to spawn a noise map to the center of the screen
+/// Plugin to generate noise map
 pub struct NoiseMapPlugin;
 
 impl Plugin for NoiseMapPlugin {
     fn build(&self, app: &mut App) {
-        app.init_resource::<NoiseMapConfig>().add_system(draw_map);
+        app.add_system(generate_map);
     }
 }
 
 /// Marker component to query noise map
 #[derive(Component)]
-pub struct NoiseMap;
+pub struct NoiseMap {
+    /// Size of the map
+    pub size: [u32; 2],
+    /// Seed of the generated noise map
+    pub seed: u32,
+    /// Scale of the generated noise map
+    pub scale: f64,
+    /// Method used to generate noise map
+    pub method: Method,
+}
+
+impl Default for NoiseMap {
+    fn default() -> Self {
+        Self {
+            size: [100; 2],
+            seed: 0,
+            scale: 0.04,
+            method: Method::Perlin,
+        }
+    }
+}
 
 /// 2 dimensional noise method used to generate noise map
 pub enum Method {
@@ -55,99 +73,32 @@ pub enum Method {
     Worley,
 }
 
-/// Resource to configure noise map. Default values are used if the resource is not inserted.
-/// # Example
-/// Insert this resource in your bevy app like so:
-/// ```
-/// use bevy::prelude::*;
-/// use bevy_generative::noise_map::{Method, NoiseMapConfig, NoiseMapPlugin};
-///
-/// fn main() {
-///     App::new()
-///         .add_plugins(DefaultPlugins)
-///         .add_plugin(NoiseMapPlugin)
-///         .insert_resource(NoiseMapConfig {
-///             method: Method::OpenSimplex,
-///             scale: 0.04,
-///             size: [100; 2],
-///             seed: 0,
-///         })
-///         .run();
-/// }
-/// ```
-/// PS: Remember to spawn a camera!
-#[derive(Resource)]
-pub struct NoiseMapConfig {
-    /// Size of the map
-    pub size: [u32; 2],
-    /// Size of the generated noise map
-    pub seed: u32,
-    /// Scale of the generated noise map
-    pub scale: f64,
-    /// Method used to generate noise map
-    pub method: Method,
-}
-
-impl Default for NoiseMapConfig {
-    fn default() -> Self {
-        Self {
-            size: [100; 2],
-            seed: 0,
-            scale: 0.04,
-            method: Method::Perlin,
-        }
-    }
-}
-
-fn draw_map(
-    noise_map_config: Res<NoiseMapConfig>,
+fn generate_map(
     mut images: ResMut<Assets<Image>>,
-    mut query: Query<(&mut UiImage, &mut Style), With<NoiseMap>>,
+    mut query: Query<(&mut UiImage, &mut Style, &NoiseMap)>,
 ) {
-    let handle = images.add(Image::new_fill(
-        Extent3d {
-            width: noise_map_config.size[0],
-            height: noise_map_config.size[1],
-            ..default()
-        },
-        TextureDimension::D2,
-        &[255, 255, 255, 255],
-        TextureFormat::Rgba8UnormSrgb,
-    ));
-
-    let mut image_buffer = images
-        .get(&handle)
-        .unwrap()
-        .clone()
-        .try_into_dynamic()
-        .expect("Texture format not supported")
-        .to_rgb8();
-
-    let noise_space = generate_noise_by_method(
-        &noise_map_config.method,
-        noise_map_config.size,
-        noise_map_config.seed,
-        noise_map_config.scale,
-    );
-
-    for x in 0..noise_map_config.size[0] {
-        for y in 0..noise_map_config.size[1] {
-            let color = noise_space[x as usize][y as usize].mul_add(255.0, -1.0) as u8;
-            image_buffer.put_pixel(x, y, image::Rgb([color; 3]));
+    for (mut ui_image, mut style, noise_map) in query.iter_mut() {
+        let mut image_buffer = image::RgbImage::new(noise_map.size[0], noise_map.size[1]);
+        let noise_space = generate_noise_by_method(
+            &noise_map.method,
+            noise_map.size,
+            noise_map.seed,
+            noise_map.scale,
+        );
+        for x in 0..image_buffer.width() {
+            for y in 0..image_buffer.height() {
+                let color = noise_space[x as usize][y as usize].mul_add(255.0, -1.0) as u8;
+                image_buffer.put_pixel(x, y, image::Rgb([color; 3]));
+            }
         }
-    }
-
-    let handle = images.add(
-        Image::from_dynamic(DynamicImage::ImageRgb8(image_buffer.clone()), true)
-            .convert(TextureFormat::Rgba8UnormSrgb)
-            .expect("Could not convert to Rgba8UnormSrgb"),
-    );
-
-    for (mut ui_image, mut style) in query.iter_mut() {
-        ui_image.texture = handle.clone();
+        ui_image.texture = images.add(
+            Image::from_dynamic(image_buffer.into(), true)
+                .convert(TextureFormat::Rgba8UnormSrgb)
+                .expect("Could not convert to Rgba8UnormSrgb"),
+        );
         style.size = Size {
-            width: Val::Px(noise_map_config.size[0] as f32),
-            height: Val::Px(noise_map_config.size[1] as f32),
+            width: Val::Px(noise_map.size[0] as f32),
+            height: Val::Px(noise_map.size[1] as f32),
         };
     }
 }
