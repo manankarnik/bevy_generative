76a510799dbcd4786fef6268f09f85063356c593
Shane Celis <shane.celis@gmail.com>
2024-03-10 20:00:04 -0400

    style: Run cargo fmt.

--- examples/map_and_textures.rs
+++ examples/map_and_textures.rs
@@ -1,13 +1,16 @@
 use bevy::prelude::*;
-use bevy::{window::PrimaryWindow, render::view::screenshot::ScreenshotManager};
+use bevy::{render::view::screenshot::ScreenshotManager, window::PrimaryWindow};
 use bevy_generative::map::{MapBundle, MapPlugin};
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins.set(WindowPlugin {
-                primary_window: Some(Window {
-                    resolution: (400., 400.).into(), ..default()
-                }), ..default()}))
+            primary_window: Some(Window {
+                resolution: (400., 400.).into(),
+                ..default()
+            }),
+            ..default()
+        }))
         .add_plugins(MapPlugin)
         .add_systems(Startup, setup)
         .add_systems(Update, screenshot_on_spacebar)
--- examples/terrain.rs
+++ examples/terrain.rs
@@ -1,18 +1,20 @@
 use bevy::prelude::*;
-use bevy::{window::PrimaryWindow, render::view::screenshot::ScreenshotManager};
-use bevy_generative::terrain::{TerrainBundle, Terrain, TerrainPlugin};
+use bevy::{render::view::screenshot::ScreenshotManager, window::PrimaryWindow};
+use bevy_generative::terrain::{Terrain, TerrainBundle, TerrainPlugin};
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins.set(WindowPlugin {
-                primary_window: Some(Window {
-                    resolution: (400., 400.).into(), ..default()
-                }), ..default()}))
+            primary_window: Some(Window {
+                resolution: (400., 400.).into(),
+                ..default()
+            }),
+            ..default()
+        }))
         .add_plugins(TerrainPlugin)
         .add_systems(Startup, setup)
         .add_systems(Update, screenshot_on_spacebar)
         .run()
-
 }
 
 fn screenshot_on_spacebar(
@@ -39,7 +41,10 @@ fn setup(mut commands: Commands) {
         ..default()
     });
     commands.spawn(TerrainBundle {
-        terrain: Terrain { wireframe: true, .. default() },
-        .. default()
+        terrain: Terrain {
+            wireframe: true,
+            ..default()
+        },
+        ..default()
     });
 }
